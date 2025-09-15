f7afa16199afc3d6a474fe4d668e9118f979a051
maybebansky <jamesweblondon@gmail.com>
2025-09-15 17:25:25 +0100

    title

--- client/app/welcome/welcome.tsx
+++ client/app/welcome/welcome.tsx
@@ -11,43 +11,46 @@ export default function App() {
   const camera = useRef(null);
 
   return (
-    <div className="h-[90vh] w-screen bg-green-100">
-      <Canvas style={{ background: "#272730" }}>
-        <PerspectiveCamera ref={camera}>
-          <ErrorBoundary
-            fallbackRender={() => (
-              <mesh>
-                <sphereGeometry />
-                <meshBasicMaterial color="red" />
-              </mesh>
-            )}
-          >
-            <Suspense
-              fallback={
+    <div className="h-screen w-screen bg-green-100 flex flex-col">
+      <div className="h-full w-screen">
+        <Canvas style={{ background: "#272730" }}>
+          <PerspectiveCamera ref={camera}>
+            <ErrorBoundary
+              fallbackRender={() => (
                 <mesh>
                   <sphereGeometry />
-                  <meshBasicMaterial color="yellow" />
+                  <meshBasicMaterial color="red" />
                 </mesh>
-              }
+              )}
             >
-              <Loader3DTilesR3FAsset
-                pointSize={1}
-                dracoDecoderPath={
-                  "https://unpkg.com/three@0.160.0/examples/jsm/libs/draco"
+              <Suspense
+                fallback={
+                  <mesh>
+                    <sphereGeometry />
+                    <meshBasicMaterial color="yellow" />
+                  </mesh>
                 }
-                basisTranscoderPath={
-                  "https://unpkg.com/three@0.160.0/examples/jsm/libs/basis"
-                }
-                rotation={new Euler(-Math.PI / 2, 0, 0)}
-                url="https://storage.googleapis.com/xoxo-test-bucket-1/chunk2/tileset.json"
-                maximumScreenSpaceError={48}
-                resetTransform={true}
-              />
-            </Suspense>
-          </ErrorBoundary>
-        </PerspectiveCamera>
-        <OrbitControls camera={camera.current} />
-      </Canvas>
+              >
+                <Loader3DTilesR3FAsset
+                  pointSize={1}
+                  dracoDecoderPath={
+                    "https://unpkg.com/three@0.160.0/examples/jsm/libs/draco"
+                  }
+                  basisTranscoderPath={
+                    "https://unpkg.com/three@0.160.0/examples/jsm/libs/basis"
+                  }
+                  rotation={new Euler(-Math.PI / 2, 0, 0)}
+                  url="https://storage.googleapis.com/xoxo-test-bucket-1/chunk2/tileset.json"
+                  maximumScreenSpaceError={48}
+                  resetTransform={true}
+                />
+              </Suspense>
+            </ErrorBoundary>
+          </PerspectiveCamera>
+          <OrbitControls camera={camera.current} />
+        </Canvas>
+      </div>
+      <p className="pl-2">three-loader-3dtiles</p>
     </div>
   );
 }
